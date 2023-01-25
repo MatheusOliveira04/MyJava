c02c5f67293bdd340060b287dc5680a47efc39eb
MatheusOliveira04 <samatheus999@gmail.com>
2023-01-25 18:51:42 -0300

    adicionado ProgramacaoFuncional_E_ExpressoesLambda

---
+++ ProgramacaoFuncional_E_ExpressoesLambda/Comparator/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.0));
+		list.add(new Product("Notebook", 1200.0));
+		list.add(new Product("Tablet", 450.0));
+		
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+	
+		list.sort(comp); // or/ new MyComparator()
+		
+		for(Product c : list) {
+			System.out.println(c);
+		}
+	}
+
+}
